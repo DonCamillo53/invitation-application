49b538d599ae5634c001a1f2ee7557ff1f5f6e9a
Camillo Hochberg <camillo.h@hotmail.de>
2024-06-23 23:05:58 +0200

    fix bug

---
+++ src/pages/eventWebsite/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const EventWebsiteIndex = () => {
+  return (
+    <div>
+      <h1>Welcome to the Event Website</h1>
+      {/* Additional content can go here */}
+    </div>
+  );
+};
+
+export default EventWebsiteIndex;
