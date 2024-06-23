6a353f492bfa9d5b429f5107f4d5dd4ed90ad9a9
Camillo Hochberg <camillo.h@hotmail.de>
2024-06-23 22:02:12 +0200

    add fix SWR remove from editor

--- src/pages/event/index.js
+++ src/pages/event/index.js
@@ -1,9 +1,7 @@
 import { HTMLeditor } from "../../../components/Emails/HTMLeditor";
-import useSWR from "swr";
 import { Layout } from "../../../components/Layout/Layout";
-export default function invitationEditor() {
-  const { mutate } = useSWR("/api/emailDraft");
 
+export default function invitationEditor() {
   async function handleSavingEmailDraft(emailDraft) {
     const response = await fetch(`/api/emailDraft`, {
       method: "PUT",
@@ -13,7 +11,10 @@ export default function invitationEditor() {
       body: JSON.stringify(emailDraft),
     });
     if (response.ok) {
-      mutate();
+      // You might want to perform some action here if the response is ok
+      console.log("Draft saved successfully.");
+    } else {
+      console.error("Failed to save the draft.");
     }
   }
 
