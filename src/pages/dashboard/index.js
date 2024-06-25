8034181ef06bf8f7484eb6a8c4254f24dcc5ef25
Camillo Hochberg <camillo.h@hotmail.de>
2024-06-25 15:33:23 +0200

    fix banner bug eventWebsite

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,36 +2,50 @@ import { useSession } from "next-auth/react";
 import { Layout } from "../../../components/Layout/Layout";
 import Countdown from "../../../components/DashboardWidgets/Countdown";
 import useSWR from "swr";
+import ContactsSum from "../../../components/DashboardWidgets/ContactsSum";
 
 export default function Dashboard() {
-  const targetDate = "2024-12-31T23:59:59";
+  const { data: contactsData, error: contactsError } = useSWR("/api/contacts");
+  const { data: emailData, error: emailError } = useSWR("/api/emailDraft");
 
-  const { data, error, loading } = useSWR("/api/emailDraft");
-
-  if (!data && !error) {
+  if (!contactsData && !contactsError && !emailData && !emailError) {
     return (
       <div className="main_container_loading">
         <div className="floating_headline_loading"></div>
       </div>
     );
   }
-  if (loading) {
+
+  if (contactsError || emailError) {
     return (
       <div className="main_container_loading">
         <div className="floating_headline_loading"></div>
+        <p>Failed loading data...</p>
       </div>
     );
   }
-  const emailData = data[0];
-  console.log(emailData.date, emailData.time);
+
+  if (!contactsData || !emailData) {
+    return (
+      <div className="main_container_loading">
+        <div className="floating_headline_loading"></div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  const contactData = contactsData;
+  const emailDetails = emailData[0];
+  console.log(emailDetails.date, emailDetails.time);
+  console.log(contactData);
+
   return (
     <div className="main_container">
       <h2 className="floating_headline">Dashboard</h2>
       <Layout />
       <div className="dashboard_widgets">
-        {emailData.date && emailData.time && (
-          <Countdown targetDate={`${emailData.date}T${emailData.time}`} />
-        )}
+        <Countdown targetDate={`${emailDetails.date}T${emailDetails.time}`} />
+        <ContactsSum data={contactData} />
       </div>
     </div>
   );
